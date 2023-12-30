dfea2bdab6b0124496be6fcf203af00cbf141883
Lisandro Baldoma <baldomalisandro@hotmail.com>
2023-12-30 15:44:01 -0300

    prueba send email

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -12,11 +12,7 @@ export async function sendEmailVerification({name, email, token}){
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        },
-        tls:{
-            rejectUnAuthorized:false
-        }
-        
+        },       
             
     }
 
