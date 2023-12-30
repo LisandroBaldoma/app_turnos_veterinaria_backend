de983593f892ab2c351d794c5c2aba7eb811f890
Lisandro Baldoma <baldomalisandro@hotmail.com>
2023-12-30 12:20:35 -0300

    email fixed

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -4,29 +4,47 @@ import { createTransport } from '../config/nodemailer.js'
 export async function sendEmailVerification({name, email, token}){  
     
     const data = {
-        host: process.env.EMAIL_HOST,
+        service: 'gmail',
+        // host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        secure: false,        
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        },
+        tls:{
+            rejectUnAuthorized:false
+        }
+        
+            
     }
+
+    console.log('DATAAAA' ,data)
     
     const transporter = createTransport(
         data
     )    
     
-    const info = await transporter.sendMail({
-        from: 'Veterinaria <cuentas@veterinaria.com>',
-        to: email,
-        subject: 'Veterinaria - Confirma tu cuenta',
-        text:"Veterinaria - Confirma tu cuenta",
-        html: `<p>Hola: ${name}, confirma tu cuenta en Veterianria</p>
-            <p>Tu cuenta esta casi listam sole debes confirmarla en el siguiente enlace</p>
-            <a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta/${token}">Confirmar cuenta</a>
-            <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje </p>
-        `
-    })
+        const info = await transporter.sendMail({
+            from: 'Veterinaria <lbaldomajubbler@gmail.com>',
+            to: email,
+            subject: 'Veterinaria - Confirma tu cuenta',
+            text:"Veterinaria - Confirma tu cuenta",
+            html: `<p>Hola: ${name}, confirma tu cuenta en Veterianria</p>
+                <p>Tu cuenta esta casi listam sole debes confirmarla en el siguiente enlace</p>
+                <a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta/${token}">Confirmar cuenta</a>
+                <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje </p>
+            `
+        })
 
-     console.log('Mensaje enviado', info.messageId)
+        
+        console.log('Mensaje enviado', info.messageId)
+
+        console.log('Mensaje enviado', info)
+        
+    
+
+     
 }
 
 export async function sendEmailPasswordReset({name, email, token}){  
